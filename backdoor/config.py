5d5c9082286862a4442646b42fc9f8e6b143b5f2
root <young_uno1080@yahoo.com>
2018-05-20 23:00:44 -0700

    Fix config file

--- backdoor/config.py
+++ backdoor/config.py
@@ -1,7 +1,10 @@
 portKnocks = [1111, 2222, 3333]
 delay = 1
 listenPort = 8005
-localPort = 8505
-remotePort = 8506
-localIP = "192.168.0.21"
-remoteIP = "192.168.0.22"
+localPort = 8506
+remotePort = 8505
+localIP = "192.168.0.22"
+remoteIP = "192.168.0.21"
+protocol = "tcp"
+password = "comp8505"
+ttl = 5
